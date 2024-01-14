3fa33f9f96f00f8080e781022e28919a5229e04f
saidheerajmiriyala <150943892+saidheerajmiriyala@users.noreply.github.com>
2024-01-15 03:18:54 +0530

    KNN
    working now

--- improvements/imcomplete.cu
+++ improvements/imcomplete.cu
@@ -1,45 +1,49 @@
-// cuda streams used here is 4. 
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
 #include<cuda.h>
 #include <time.h>
 
-#define K 1 // k for KNN 
-
-
+#define K 7
+#define  noofdimensions 784
 #define TRAIN_SIZE (60000)
 #define TEST_SIZE (10000)
+
 
 
 typedef struct {
     int out;
     double *feutures;                   
 } record_t;
-//struct for storing  each datapoint in datasets, out is class , feutures is pointer to feuture array
+
 
 
 typedef struct {
     int out;
     double distance;                   
 } distance_t;
-//struct for storing distance value and corresponding class label after findind distance between two points
+
 
 typedef struct {
     distance_t *dist_s0; 
     distance_t *dist_s1;
     distance_t *dist_s2;
     distance_t *dist_s3;
+    distance_t *dist_s4;
+    distance_t *dist_s5;
 } final_t;
-//struct of type final_t contains 4 arrays of distances from each 4 streams
+
 
 typedef struct{
     distance_t *arr_s0;
     distance_t *arr_s1;
     distance_t *arr_s2;
     distance_t *arr_s3;
+    distance_t *arr_s4;
+    distance_t *arr_s5;
+
 }tempstruct;
-//tempporary struct to trim the distances arrays to only first K smallest elements for each testpoint
+
 
 
 typedef struct{
@@ -47,11 +51,6 @@ typedef struct{
     int *arrfinal;
     int *freq;
 }tempstruct2;
-//temporary struct used by sorting kernel for final kernel for each test point
-
-
-
-
 
 
 __device__ void selection_sort(distance_t arr[], int n) {
@@ -74,24 +73,10 @@ __device__ void selection_sort(distance_t arr[], int n) {
         }
     }
 }
-//selection sort limited to first k iterations leading to first k smallest elements
 
-
-
-
-
-
-
-
-
-
-
-
-
-//euclidean distance
 __device__ double distanceBetweenImages_e(double *pixels1, double *pixels2) {
     double dist = 0;
-    for (int i = 0; i < 784; i++) {
+    for (int i = 0; i < noofdimensions; i++) {
         double diff = pixels1[i] - pixels2[i];
         dist += diff*diff;
     }
@@ -99,10 +84,10 @@ __device__ double distanceBetweenImages_e(double *pixels1, double *pixels2) {
 }
 
 
-//manhatan distance
+
 __device__ double distanceBetweenImages_m(double *pixels1, double *pixels2) {
     double dist = 0;
-    for (int i = 0; i < 784; i++) {
+    for (int i = 0; i < noofdimensions; i++) {
         double diff = pixels1[i] - pixels2[i];
         if(diff<0){
         diff=-1*diff;}
@@ -113,8 +98,7 @@ __device__ double distanceBetweenImages_m(double *pixels1, double *pixels2) {
 
 
 
-//to free the  device allocated arrays
-void free_arrays(record_t images[], int num_images) {
+void free_images(record_t images[], int num_images) {
     for (int i = 0; i < num_images; i++) {
         cudaFree(images[i].feutures);
     }
@@ -122,31 +106,99 @@ void free_arrays(record_t images[], int num_images) {
 
 
 
-
-//distance kernel that uses grid stride loop 
-__global__ void distance_kernel(record_t *train_data, record_t test_point, distance_t *distancesss,int size) {
+__global__ void knn_slaves0(record_t *train_data, record_t *test_point, final_t *final_dist_all,int size) {
     int tid = threadIdx.x + blockIdx.x * blockDim.x;
     int stride = gridDim.x * blockDim.x;
-    for (int i = tid; i < size; i+=stride) {  distancesss[i].distance=distanceBetweenImages_e(train_data[i].feutures,test_point.feutures);
-    distancesss[i].out=train_data[i].out;
+    for (int mm = tid; mm < TEST_SIZE; mm+=stride) { 
+    for(int i =0;i<size;i++){
+    final_dist_all[mm].dist_s0[i].distance=distanceBetweenImages_e(train_data[i].feutures,test_point[mm].feutures);
+    final_dist_all[mm].dist_s0[i].out=train_data[i].out;
+    }
     
 }
 
 }
 
 
-//sorting kernel
-__global__ void sorting_kernel(tempstruct2 *f4karrays,tempstruct *allarrays,final_t *final_dist_all, int N, int *results,int t_s0){
+__global__ void knn_slaves1(record_t *train_data, record_t *test_point, final_t *final_dist_all,int size) {
+    int tid = threadIdx.x + blockIdx.x * blockDim.x;
+    int stride = gridDim.x * blockDim.x;
+    for (int mm = tid; mm < TEST_SIZE; mm+=stride) { 
+    for(int i =0;i<size;i++){
+    final_dist_all[mm].dist_s1[i].distance=distanceBetweenImages_e(train_data[i].feutures,test_point[mm].feutures);
+    final_dist_all[mm].dist_s1[i].out=train_data[i].out;
+    }
+    
+}
+
+}
+
+__global__ void knn_slaves2(record_t *train_data, record_t *test_point, final_t *final_dist_all,int size) {
+    int tid = threadIdx.x + blockIdx.x * blockDim.x;
+    int stride = gridDim.x * blockDim.x;
+    for (int mm = tid; mm < TEST_SIZE; mm+=stride) { 
+    for(int i =0;i<size;i++){
+    final_dist_all[mm].dist_s2[i].distance=distanceBetweenImages_e(train_data[i].feutures,test_point[mm].feutures);
+    final_dist_all[mm].dist_s2[i].out=train_data[i].out;
+    }
+    
+}
+
+}
+
+__global__ void knn_slaves3(record_t *train_data, record_t *test_point, final_t *final_dist_all,int size) {
+    int tid = threadIdx.x + blockIdx.x * blockDim.x;
+    int stride = gridDim.x * blockDim.x;
+    for (int mm = tid; mm < TEST_SIZE; mm+=stride) { 
+    for(int i =0;i<size;i++){
+    final_dist_all[mm].dist_s3[i].distance=distanceBetweenImages_e(train_data[i].feutures,test_point[mm].feutures);
+    final_dist_all[mm].dist_s3[i].out=train_data[i].out;
+    }
+    
+}
+
+}
+
+
+
+__global__ void knn_slaves4(record_t *train_data, record_t *test_point, final_t *final_dist_all,int size) {
+    int tid = threadIdx.x + blockIdx.x * blockDim.x;
+    int stride = gridDim.x * blockDim.x;
+    for (int mm = tid; mm < TEST_SIZE; mm+=stride) { 
+    for(int i =0;i<size;i++){
+    final_dist_all[mm].dist_s4[i].distance=distanceBetweenImages_e(train_data[i].feutures,test_point[mm].feutures);
+    final_dist_all[mm].dist_s4[i].out=train_data[i].out;
+    }
+    
+}
+
+}
+
+
+__global__ void knn_slaves5(record_t *train_data, record_t *test_point, final_t *final_dist_all,int size) {
+    int tid = threadIdx.x + blockIdx.x * blockDim.x;
+    int stride = gridDim.x * blockDim.x;
+    for (int mm = tid; mm < TEST_SIZE; mm+=stride) { 
+    for(int i =0;i<size;i++){
+    final_dist_all[mm].dist_s5[i].distance=distanceBetweenImages_e(train_data[i].feutures,test_point[mm].feutures);
+    final_dist_all[mm].dist_s5[i].out=train_data[i].out;
+    }
+    
+}
+
+}
+
+
+__global__ void finalkernel(tempstruct2 *f4karrays,tempstruct *allarrays,final_t *final_dist_all, int N, int *results,int t_s0){
     int tid = threadIdx.x + blockIdx.x * blockDim.x;
     int stride = gridDim.x * blockDim.x;
     for (int mm = tid; mm < N; mm+=stride){
-      //each thread sorts using k limit selection sort
-      //each thread sorts all distance arrays sequencially
     selection_sort(final_dist_all[mm].dist_s0, t_s0);
     selection_sort(final_dist_all[mm].dist_s1, t_s0);
     selection_sort(final_dist_all[mm].dist_s2, t_s0);
     selection_sort(final_dist_all[mm].dist_s3, t_s0);
-    //takes the first k elements from each distance arrays and combine to get array of 4K
+    selection_sort(final_dist_all[mm].dist_s4, t_s0);
+    selection_sort(final_dist_all[mm].dist_s5, t_s0);
     for(int ll=0;ll<K;ll++){
     allarrays[mm].arr_s0[ll]=final_dist_all[mm].dist_s0[ll];}
         for(int ll=0;ll<K;ll++){
@@ -155,6 +207,10 @@ __global__ void sorting_kernel(tempstruct2 *f4karrays,tempstruct *allarrays,fina
     allarrays[mm].arr_s2[ll]=final_dist_all[mm].dist_s2[ll];}
         for(int ll=0;ll<K;ll++){
     allarrays[mm].arr_s3[ll]=final_dist_all[mm].dist_s3[ll];}
+    for(int ll=0;ll<K;ll++){
+    allarrays[mm].arr_s4[ll]=final_dist_all[mm].dist_s4[ll];}
+    for(int ll=0;ll<K;ll++){
+    allarrays[mm].arr_s5[ll]=final_dist_all[mm].dist_s5[ll];}
     int i, j = 0;
     for (i = 0; i < K; i++) {
         f4karrays[mm].arr[j++] = allarrays[mm].arr_s0[i];
@@ -168,7 +224,13 @@ __global__ void sorting_kernel(tempstruct2 *f4karrays,tempstruct *allarrays,fina
     for (i = 0; i < K; i++) {
         f4karrays[mm].arr[j++] = allarrays[mm].arr_s3[i];
     }  
-    selection_sort(f4karrays[mm].arr, 4*K);
+    for (i = 0; i < K; i++) {
+        f4karrays[mm].arr[j++] = allarrays[mm].arr_s4[i];
+    } 
+    for (i = 0; i < K; i++) {
+        f4karrays[mm].arr[j++] = allarrays[mm].arr_s5[i];
+    }
+    selection_sort(f4karrays[mm].arr, 6*K);
     for(int ll=0;ll<K;ll++){
     f4karrays[mm].arrfinal[ll]=f4karrays[mm].arr[ll].out;}
     // int freq[10] = {0};
@@ -192,18 +254,19 @@ __global__ void sorting_kernel(tempstruct2 *f4karrays,tempstruct *allarrays,fina
 
 }
 
+
+
+
 int main() {
-  //row is train set
-  //rowt is test dataset
     FILE *fp;
     int n = 60000;
-    
+
         record_t *row;
     cudaMallocManaged(&row, n * sizeof(record_t));
         for (int k = 0; k < n; k++) {
     cudaMallocManaged(&(row[k].feutures), 784 * sizeof(double));
 }
-   
+
 
 
 
@@ -234,19 +297,21 @@ do {
 } while (!feof(fp)); 
         fclose(fp);
         
-        printf("\\\\n%d records read.\\\\n\\\\n", records);  
+        printf("\\\\n%d records read.\\\\n\\\\n", records-1);  
 
 
 
-     
 
-             record_t *rowt;
-    cudaMallocManaged(&rowt, 10000 * sizeof(record_t));
-        for (int k = 0; k < 10000; k++) {
+n2=10000;
+        record_t *rowt;
+    cudaMallocManaged(&rowt, n2 * sizeof(record_t));
+        for (int k = 0; k < n2; k++) {
     cudaMallocManaged(&(rowt[k].feutures), 784 * sizeof(double));
 }
-        
-           fp = fopen("mnist_test.csv", "r");
+
+
+
+    fp = fopen("mnist_test.csv", "r");
     if (fp == NULL) {
         fprintf(stderr, "Error reading file\\\\n");
         return 1;
@@ -256,9 +321,10 @@ do {
      read=0;
   
 do {
-  read = fscanf(fp, "%d,", &rowt[records].out);
+  read = fscanf(fp, "%d,", &row[records].out);
+
   for (int i = 0; i < 784; i++) {
-    read = fscanf(fp, "%lf,", &rowt[records].feutures[i]);
+    read = fscanf(fp, "%lf,", &row[records].feutures[i]);
   }
 
  
@@ -272,67 +338,64 @@ do {
 } while (!feof(fp)); 
         fclose(fp);
         
-        printf("\\\\n%d records read.\\\\n\\\\n", records);
-       
-       
-       struct timespec start, end;
-clock_gettime(CLOCK_MONOTONIC, &start);
+        printf("\\\\n%d records read.\\\\n\\\\n", records - 1);  
 
-        
-           
 
- //streams
+n1=60000;
 cudaStream_t s1;
 cudaStream_t s2;
 cudaStream_t s3;
+cudaStream_t s4;
+cudaStream_t s5;
 cudaStreamCreate(&s1);
 cudaStreamCreate(&s2);
 cudaStreamCreate(&s3);
+cudaStreamCreate(&s4);
+cudaStreamCreate(&s5);
 
-int n1=60000;
         final_t *final_dist_all;
     cudaMallocManaged(&final_dist_all, TEST_SIZE * sizeof(final_t));
         for (int k = 0; k < TEST_SIZE; k++) {
-    cudaMallocManaged(&(final_dist_all[k].dist_s0), (n1/4) * sizeof(distance_t));
-    cudaMallocManaged(&(final_dist_all[k].dist_s1), (n1/4) * sizeof(distance_t));
-    cudaMallocManaged(&(final_dist_all[k].dist_s2), (n1/4) * sizeof(distance_t));
-    cudaMallocManaged(&(final_dist_all[k].dist_s3), (n1/4) * sizeof(distance_t));
+    cudaMallocManaged(&(final_dist_all[k].dist_s0), (n1/6) * sizeof(distance_t));
+    cudaMallocManaged(&(final_dist_all[k].dist_s1), (n1/6) * sizeof(distance_t));
+    cudaMallocManaged(&(final_dist_all[k].dist_s2), (n1/6) * sizeof(distance_t));
+    cudaMallocManaged(&(final_dist_all[k].dist_s3), (n1/6) * sizeof(distance_t));
+    cudaMallocManaged(&(final_dist_all[k].dist_s4), (n1/6) * sizeof(distance_t));
+    cudaMallocManaged(&(final_dist_all[k].dist_s5), (n1/6) * sizeof(distance_t));
 }
 
 
 
 
 
-int t_s1=(n1/4);
-int t_s2=(n1/4);
-int t_s3=(n1/4);
-int t_s0=(n1/4);
-// record_t *part1_s1;
-// record_t *part1_s2;
-// record_t *part1_s3;
-// record_t *part1_s0;
-// cudaMallocManaged(&part1_s1, t_s1 * sizeof(record_t));
-// cudaMallocManaged(&part1_s2, t_s2 * sizeof(record_t));
-// cudaMallocManaged(&part1_s3, t_s3 * sizeof(record_t));
-// cudaMallocManaged(&part1_s0, t_s0 * sizeof(record_t));
-
-
-
-// cudaMemcpy(part1_s0, row, t_s1 * sizeof(record_t), cudaMemcpyDefault);
-// cudaMemcpy(part1_s1, row + t_s1, t_s1 * sizeof(record_t), cudaMemcpyDefault);
-// cudaMemcpy(part1_s2, row + 2 * t_s1, t_s1 * sizeof(record_t), cudaMemcpyDefault);
-// cudaMemcpy(part1_s3, row + 3 * t_s1, t_s1 * sizeof(record_t), cudaMemcpyDefault);
-    
+int t_s1=(n1/6);
+int t_s2=(n1/6);
+int t_s3=(n1/6);
+int t_s0=(n1/6);
 
 
 int no_correct=0;
-for(int mm=0;mm<(10000);mm++){
-    distance_kernel<<<20,64>>>(row, rowt[mm],final_dist_all[mm].dist_s0,t_s0);
-    distance_kernel<<<20,64,0,s1>>>(row+t_s1, rowt[mm],final_dist_all[mm].dist_s1,t_s1);
-    distance_kernel<<<20,64,0,s2>>>(row+2*t_s1, rowt[mm],final_dist_all[mm].dist_s2,t_s1);
-    distance_kernel<<<20,64,0,s3>>>(row+3*t_s1, rowt[mm],final_dist_all[mm].dist_s3,t_s1);
-        }
-cudaDeviceSynchronize();
+
+
+struct timespec start, end;
+clock_gettime(CLOCK_MONOTONIC, &start);
+
+for(int i=0; i<10000;i++){
+
+    knn_slaves1<<<20,64,0,s1>>>(row, rowt[i],final_dist_all,t_s1);
+    knn_slaves2<<<20,64,0,s2>>>(row+(n1/6), rowt[i],final_dist_all,t_s1);
+    knn_slaves3<<<20,64,0,s3>>>(row+2*(n1/6), rowt[i],final_dist_all,t_s1);
+    knn_slaves4<<<20,64,0,s4>>>(row+2*(n1/6), rowt[i],final_dist_all,t_s1);
+    knn_slaves5<<<20,64,0,s5>>>(row+4*(n1/6), rowt[i],final_dist_all,t_s1);
+    knn_slaves0<<<20,64>>>(row+5*(n1/6), rowt[i],final_dist_all,t_s1);
+    
+    }
+        
+
+
+
+
+
 
 
 
@@ -340,7 +403,7 @@ cudaDeviceSynchronize();
 
 
 int *results;
-cudaMallocManaged(&results, (10000) * sizeof(int));
+cudaMallocManaged(&results, (noofelements-n1) * sizeof(int));
         tempstruct *allarrays;
     cudaMallocManaged(&allarrays, TEST_SIZE * sizeof(tempstruct));
         for (int k = 0; k < TEST_SIZE; k++) {
@@ -348,24 +411,24 @@ cudaMallocManaged(&results, (10000) * sizeof(int));
     cudaMallocManaged(&(allarrays[k].arr_s1), (K) * sizeof(distance_t));
     cudaMallocManaged(&(allarrays[k].arr_s2), (K) * sizeof(distance_t));
     cudaMallocManaged(&(allarrays[k].arr_s3), (K) * sizeof(distance_t));
+    cudaMallocManaged(&(allarrays[k].arr_s4), (K) * sizeof(distance_t));
+    cudaMallocManaged(&(allarrays[k].arr_s5), (K) * sizeof(distance_t));
 }
-
-
 
         tempstruct2 *f4karrays;
     cudaMallocManaged(&f4karrays, TEST_SIZE * sizeof(tempstruct2));
         for (int k = 0; k < TEST_SIZE; k++) {
-    cudaMallocManaged(&(f4karrays[k].arr), (4*K) * sizeof(distance_t));
+    cudaMallocManaged(&(f4karrays[k].arr), (6*K) * sizeof(distance_t));
     cudaMallocManaged(&(f4karrays[k].arrfinal), (K) * sizeof(int));
     cudaMallocManaged(&(f4karrays[k].freq), (10) * sizeof(int));
 }
-
-sorting_kernel<<<20,256>>>(f4karrays,allarrays,final_dist_all,(10000),results, t_s0);
+cudaDeviceSynchronize();
+finalkernel<<<45,256>>>(f4karrays,allarrays,final_dist_all,(noofelements-n1),results, t_s0);
 
 cudaDeviceSynchronize();
-for(int mmm=0;mmm<(10000);mmm++){
-    
-    if(results[mmm]==rowt[mmm].out){
+for(int mmm=0;mmm<(noofelements-n1);mmm++){
+    printf("%d,%d\n",results[mmm],part2[mmm].out);
+    if(results[mmm]==part2[mmm].out){
         no_correct++;
     }
 }
@@ -380,17 +443,20 @@ for(int mmm=0;mmm<(10000);mmm++){
 cudaStreamDestroy(s1);
 cudaStreamDestroy(s2);
 cudaStreamDestroy(s3);
-
-
-    free_arrays(row,60000);
-    free_arrays(rowt,10000);
+cudaStreamDestroy(s4);
+cudaStreamDestroy(s5);
+    free_images(row,60000);
     cudaFree(row);
+    free_images(rowt,10000);
     cudaFree(rowt);
+    
     for (int k = 0; k < TEST_SIZE; k++) {
     cudaFree(final_dist_all[k].dist_s0);
     cudaFree(final_dist_all[k].dist_s1);
     cudaFree(final_dist_all[k].dist_s2);
     cudaFree(final_dist_all[k].dist_s3);
+    cudaFree(final_dist_all[k].dist_s4);
+    cudaFree(final_dist_all[k].dist_s5);
 }
 
 cudaFree(final_dist_all);
@@ -408,7 +474,14 @@ for (int k = 0; k < TEST_SIZE; k++) {
     cudaFree(allarrays[k].arr_s1);
     cudaFree(allarrays[k].arr_s2);
     cudaFree(allarrays[k].arr_s3);
+    cudaFree(allarrays[k].arr_s4);
+    cudaFree(allarrays[k].arr_s5);
 }
 cudaFree(allarrays);
 cudaFree(results);
+
+
+
+    return 0;
+
 }
